06739bc9ea00e345ab030773329dd7f3f332f400
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2024-01-10 20:00:23 +0200

    Added some rules to .eslintrc

--- webpack.common.js
+++ webpack.common.js
@@ -2,29 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
+    'entry': {
+        'app': './src/index.js',
     },
-    plugins: [
+    'plugins': [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
-            title: 'App',
+            'template': './src/index.html',
+            'title': 'App',
         }),
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
+    'output': {
+        'filename': 'main.js',
+        'path': path.resolve(__dirname, 'dist'),
+        'clean': true,
     },
-    module: {
-        rules: [
+    'module': {
+        'rules': [
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                'test': /\.css$/i,
+                'use': ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                'test': /\.(png|svg|jpg|jpeg|gif)$/i,
+                'type': 'asset/resource',
             },
         ],
     },
